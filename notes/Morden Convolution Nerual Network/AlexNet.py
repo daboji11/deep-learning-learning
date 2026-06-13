28e5d27d32ce5e4ef74715906e9ae2c4b1d091fc
dabozi11 <yixiaozhang2005@gmail.com>
2026-06-13 15:20:58 +1000

    add gpu test notes

--- notes/Morden Convolution Nerual Network/AlexNet.py
+++ notes/Morden Convolution Nerual Network/AlexNet.py
@@ -8,6 +8,12 @@ from d2l import torch as d2l
 # 668.6 examples/sec on mps
 # Total time: 1111.62 seconds
 
+# with 4060 and no animator:
+# loss 0.328, train acc 0.879, test acc 0.885
+# 1569.7 examples/sec on cuda:0
+# Total time: 608.99 seconds
+
+
 def try_mps():
     if torch.backends.mps.is_available():
         return torch.device("mps")
@@ -44,41 +50,43 @@ for layer in net:
     print(layer.__class__.__name__,'output shape:\t',X.shape)
 
 batch_size = 128
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
 
-transform = transforms.Compose([
-    transforms.Resize(224),
-    transforms.ToTensor()
-])
+# from torchvision import datasets, transforms
+# from torch.utils.data import DataLoader
 
-train_data = datasets.FashionMNIST(
-    root="../data",
-    train=True,
-    transform=transform,
-    download=True
-)
+# transform = transforms.Compose([
+#     transforms.Resize(224),
+#     transforms.ToTensor()
+# ])
 
-test_data = datasets.FashionMNIST(
-    root="../data",
-    train=False,
-    transform=transform,
-    download=True
-)
+# train_data = datasets.FashionMNIST(
+#     root="../data",
+#     train=True,
+#     transform=transform,
+#     download=True
+# )
 
-train_iter = DataLoader(
-    train_data,
-    batch_size=128,
-    shuffle=True,
-    num_workers=0
-)
+# test_data = datasets.FashionMNIST(
+#     root="../data",
+#     train=False,
+#     transform=transform,
+#     download=True
+# )
 
-test_iter = DataLoader(
-    test_data,
-    batch_size=128,
-    shuffle=False,
-    num_workers=0
-)
+# train_iter = DataLoader(
+#     train_data,
+#     batch_size=128,
+#     shuffle=True,
+#     num_workers=0
+# )
+
+# test_iter = DataLoader(
+#     test_data,
+#     batch_size=128,
+#     shuffle=False,
+#     num_workers=0
+# )
 
 lr, num_epochs = 0.01, 10
 
@@ -87,7 +95,7 @@ import time
 start = time.time()
 
 
-def evaluate_accuracy_gpu(net, data_iter, device = try_mps()): #@save
+def evaluate_accuracy_gpu(net, data_iter, device = d2l.try_gpu()): #@save
     """使用GPU计算模型在数据集上的精度"""
     if isinstance(net, nn.Module):
         net.eval()  # 设置为评估模式
@@ -106,45 +114,46 @@ def evaluate_accuracy_gpu(net, data_iter, device = try_mps()): #@save
             metric.add(d2l.accuracy(net(X), y), y.numel())
     return metric[0] / metric[1]
 
-def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
-    """用GPU训练模型(在第六章定义)"""
-    def init_weights(m):
-        if type(m) == nn.Linear or type(m) == nn.Conv2d:
-            nn.init.xavier_uniform_(m.weight)
-    net.apply(init_weights)
-    print('training on', device)
-    net.to(device)
-    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
-    loss = nn.CrossEntropyLoss()
-    timer, num_batches = d2l.Timer(), len(train_iter)
-    for epoch in range(num_epochs):
-        # 训练损失之和，训练准确率之和，样本数
-        metric = d2l.Accumulator(3)
-        net.train()
-        for i, (X, y) in enumerate(train_iter):
-            print("current: ", i)
-            timer.start()
-            optimizer.zero_grad()
-            X, y = X.to(device), y.to(device)
-            y_hat = net(X)
-            l = loss(y_hat, y)
-            l.backward()
-            optimizer.step()
-            with torch.no_grad():
-                metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
-            timer.stop()
-            train_l = metric[0] / metric[2]
-            train_acc = metric[1] / metric[2]
-        test_acc = evaluate_accuracy_gpu(net, test_iter, device)
+# def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
+#     """用GPU训练模型(在第六章定义)"""
+#     def init_weights(m):
+#         if type(m) == nn.Linear or type(m) == nn.Conv2d:
+#             nn.init.xavier_uniform_(m.weight)
+#     net.apply(init_weights)
+#     print('training on', device)
+#     net.to(device)
+#     optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+#     loss = nn.CrossEntropyLoss()
+#     timer, num_batches = d2l.Timer(), len(train_iter)
+#     for epoch in range(num_epochs):
+#         # 训练损失之和，训练准确率之和，样本数
+#         metric = d2l.Accumulator(3)
+#         net.train()
+#         for i, (X, y) in enumerate(train_iter):
+#             print(epoch, '/', num_epochs, 'batch', i, '/', num_batches)
+#             timer.start()
+#             optimizer.zero_grad()
+#             X, y = X.to(device), y.to(device)
+#             y_hat = net(X)
+#             l = loss(y_hat, y)
+#             l.backward()
+#             optimizer.step()
+#             with torch.no_grad():
+#                 metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
+#             timer.stop()
+#             train_l = metric[0] / metric[2]
+#             train_acc = metric[1] / metric[2]
+#         test_acc = evaluate_accuracy_gpu(net, test_iter, device)
 
-    print(f'loss {train_l:.3f}, train acc {train_acc:.3f}, '
-          f'test acc {test_acc:.3f}')
-    print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
-          f'on {str(device)}')
+#     print(f'loss {train_l:.3f}, train acc {train_acc:.3f}, '
+#           f'test acc {test_acc:.3f}')
+#     print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
+#           f'on {str(device)}')
 
 
-train_ch6(net, train_iter, test_iter, num_epochs, lr, try_mps())
-
+# train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
+lr, num_epochs = 0.01, 10
+d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, d2l.try_gpu())
 end = time.time()
 
 print(f"Total time: {end-start:.2f} seconds")
