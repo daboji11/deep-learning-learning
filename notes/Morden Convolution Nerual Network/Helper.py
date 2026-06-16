e89947c63a90c87b7d8da4133dfddd729912c15c
dabozi11 <yixiaozhang2005@gmail.com>
2026-06-16 14:05:05 +1000

    notes for ResNet

--- notes/Morden Convolution Nerual Network/Helper.py
+++ notes/Morden Convolution Nerual Network/Helper.py
@@ -1,6 +1,6 @@
-import torch
-from torch import nn
-from d2l import torch as d2l
+# import torch
+# from torch import nn
+# from d2l import torch as d2l
 
 
 # from torchvision import datasets, transforms
